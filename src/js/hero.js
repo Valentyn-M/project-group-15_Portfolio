5e89678eafcfe4b37b905942dc84f726bcf81388
OlenaSorokina <114348764+elenka-lev@users.noreply.github.com>
2024-10-13 11:37:32 +0300

    Final version hero

---
+++ src/js/hero.js
@@ -0,0 +1,34 @@
+const subtitle = document.querySelector('.hero-subtitle');
+const text = subtitle.textContent;
+subtitle.textContent = '';
+
+const letters = Array.from(text);
+let index = 0;
+
+function showLetter() {
+    if (index < letters.length) {
+        
+        const span = document.createElement('span');
+        span.textContent = letters[index];
+        span.classList.add('show');
+        subtitle.appendChild(span);
+        index++;
+        setTimeout(showLetter, 300);
+    } else {
+        setTimeout(hideText, 2000);
+    }
+}
+
+function hideText() {
+    const spans = subtitle.querySelectorAll('span');
+    spans.forEach(span => {
+        span.classList.remove('show');
+    });
+    setTimeout(() => {
+        subtitle.textContent = '';
+        index = 0;
+        setTimeout(showLetter, 1000);
+    }, 500);
+}
+
+showLetter(); 
