73a95c49a7addf2dd05d6c05b5d347a544571549
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-10-16 00:06:03 +0200

    Finish theme + fix mobile menu

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,8 +1,8 @@
-// const htmlElement = document.documentElement;
+const htmlElement = document.documentElement;
 const themeButton = document.querySelector("#toggleLabel");
 themeButton.addEventListener("click", () => changeTheme(true));
 
-/*
+
 // Функция смены темы сайта (если функция вызывается по клику, то в аргументу функции будет true)
 function changeTheme(saveTheme = false) {
 
@@ -26,37 +26,36 @@ function changeTheme(saveTheme = false) {
 	saveTheme ? localStorage.setItem('theme', newTheme) : null;
 }
 
-// Основной код (Самовызываемая функция)
-(function () {
-	// Получаем сохраненную тему из localStorage (theme - это ключ для хранения темы, выбранной пользователем)
-	// Свойство localStorage позволяет получить доступ к Storage объекту. localStorage аналогично свойству sessionStorage. Разница только в том, что свойство sessionStorage хранит данные в течение сеанса (до закрытия браузера), в отличие от данных, находящихся в свойстве localStorage, которые не имеют ограничений по времени хранения и могут быть удалены только с помощью JavaScript.
-	// При первом заходе на сайт эта константа будет пустой. Она заполняется в  функции смены темы changeTheme
-	const saveUserTheme = localStorage.getItem('theme');
+// Основной код (Самовызываемая функция) - Перенесено в index.html
+// (function () {
+// 	// Получаем сохраненную тему из localStorage (theme - это ключ для хранения темы, выбранной пользователем)
+// 	// Свойство localStorage позволяет получить доступ к Storage объекту. localStorage аналогично свойству sessionStorage. Разница только в том, что свойство sessionStorage хранит данные в течение сеанса (до закрытия браузера), в отличие от данных, находящихся в свойстве localStorage, которые не имеют ограничений по времени хранения и могут быть удалены только с помощью JavaScript.
+// 	// При первом заходе на сайт эта константа будет пустой. Она заполняется в  функции смены темы changeTheme
+// 	const saveUserTheme = localStorage.getItem('theme');
 
-	// Работа с системными настройками
-	let userTheme;
-	if (window.matchMedia) {
-		// Заносим в переменную userTheme цветовую тему Системы пользователя
-		userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-	}
-	// Ставим прослушку на Событие изменения темы (спрослушку ставим на темную тему)
-	window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", e => {
-		// Если выбранная пользователем тема отличается от сохраненной ранее темы (if (window.matchMedia('(prefers-color-scheme: dark)').matches != saveUserTheme), то запускаем функцию изменения темы changeTheme(), в противном случае ничего не дулаем (null)
-		// Функция changeTheme() вызывается при смене Системных настроек темы, при условии, что нет сохораненных пользователем настроек темы на сайте
-		!saveUserTheme ? changeTheme() : null;
-	});
+// 	// Работа с системными настройками
+// 	let userTheme;
+// 	if (window.matchMedia) {
+// 		// Заносим в переменную userTheme цветовую тему Системы пользователя
+// 		userTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+// 	}
+// 	// Ставим прослушку на Событие изменения темы (спрослушку ставим на темную тему)
+// 	window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", e => {
+// 		// Если выбранная пользователем тема отличается от сохраненной ранее темы (if (window.matchMedia('(prefers-color-scheme: dark)').matches != saveUserTheme), то запускаем функцию изменения темы changeTheme(), в противном случае ничего не дулаем (null)
+// 		// Функция changeTheme() вызывается при смене Системных настроек темы, при условии, что нет сохораненных пользователем настроек темы на сайте
+// 		!saveUserTheme ? changeTheme() : null;
+// 	});
 
-	// Функция установки темы при загрузкe страницы (Самовызываемая функция)
-	function setThemeClass() {
-		if (saveUserTheme) {                                      													// Если пользователь ранее выбрал тему сайта (переменная saveUserTheme - не пустая)
-			htmlElement.classList.add(`_${saveUserTheme}`);                 									// Добавляем класс этой темы для HTML
-			htmlElement.setAttribute("data-color-scheme", saveUserTheme);       							// Устанавливаем атрибут "data-color-scheme" для HTML
-		} else {                                              															// Иначе
-			htmlElement.classList.add(`_${userTheme}`);                   										// Добавляем класс Системной темы для HTML
-			htmlElement.setAttribute("data-color-scheme", userTheme);         								// Устанавливаем атрибут "data-color-scheme" для HTML из системы пользователя
-		}
-	}
-	setThemeClass();
+// 	// Функция установки темы при загрузкe страницы (Самовызываемая функция)
+// 	function setThemeClass() {
+// 		if (saveUserTheme) {                                      													// Если пользователь ранее выбрал тему сайта (переменная saveUserTheme - не пустая)
+// 			htmlElement.classList.add(`_${saveUserTheme}`);                 									// Добавляем класс этой темы для HTML
+// 			htmlElement.setAttribute("data-color-scheme", saveUserTheme);       							// Устанавливаем атрибут "data-color-scheme" для HTML
+// 		} else {                                              															// Иначе
+// 			htmlElement.classList.add(`_${userTheme}`);                   										// Добавляем класс Системной темы для HTML
+// 			htmlElement.setAttribute("data-color-scheme", userTheme);         								// Устанавливаем атрибут "data-color-scheme" для HTML из системы пользователя
+// 		}
+// 	}
+// 	setThemeClass();
 
-}());
-*/
+// }());
