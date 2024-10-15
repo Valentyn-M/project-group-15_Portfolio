85473d8fc66a283f6a25793e38f1e7255c5bcb32
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-10-15 18:46:10 +0200

    Revert "Feature/faq section styles"

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-
-import './js/switch-theme';
-import './js/header';
-import './js/hero';
-import './js/about-me';
-import './js/tech-skills';
-import './js/my-projects';
-import './js/benefits';
-import './js/faq';
-import './js/reviews';
-import './js/work-together';
-import './js/footer';
-
 import "./js/switch-theme"
 import "./js/header"
 import "./js/hero"
@@ -19,4 +6,3 @@ import "./js/faq"
 import "./js/reviews"
 import "./js/work-together"
 import "./js/footer"
-
